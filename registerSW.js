01734ebb353daa257288bc83bbdbe578a018b5d0
gui130699 <gui130699@github.io>
2026-03-26 10:24:47 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/InstaSpy/sw.js', { scope: '/InstaSpy/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
